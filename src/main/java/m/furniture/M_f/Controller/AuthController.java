a8165e123f8ca562c89d1897b325441d218abfa8
Serhii Leniv <greyo2006@gmail.com>
2025-04-09 00:42:46 +0300

    front changed, call request added.

---
+++ src/main/java/m/furniture/M_f/Controller/AuthController.java
@@ -0,0 +1,33 @@
+package m.furniture.M_f.Controller;
+
+import m.furniture.M_f.Entity.User;
+import m.furniture.M_f.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        userService.registerUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+}
